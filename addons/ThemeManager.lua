e4870d45019e9d83820b154d930fe84a39d3d699
qwxyukari <halseyistrefi@gmail.com>
2026-05-04 04:13:38 +0300

    Update ThemeManager.lua

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -280,7 +280,7 @@ do
             LibraryScheme["Font"] = Font.fromEnum(Enum.Font[theme["FontFace"]])
 
         else
-            FinalTheme["FontFace"] = "Code"
+            FinalTheme["FontFace"] = "Gotham"
             LibraryScheme["Font"] = Font.fromEnum(Enum.Font.Code)
         end
 
